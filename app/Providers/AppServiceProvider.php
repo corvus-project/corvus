66669bdeeed97c4b582a967eb2f79d34eac43430
Kemal Yenilmez <kemalyen@gmail.com>
2020-03-19 22:34:02 +0000

    fix on servce

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use URL;
 
 class AppServiceProvider extends ServiceProvider
 {
